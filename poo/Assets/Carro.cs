789d182004c747ed9de7fee5c523427a5ad8a95c
Dayanne6 <d.karoline@escolar.ifrn.edu.br>
2025-04-14 09:14:52 -0300

    jjjjn

---
+++ poo/Assets/Carro.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Carro
+{
+    private string nome;
+    private int numero_de_rodas;
+    private string cor;
+    private float preço;
+
+    public string getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(string nome)
+    {
+        this.nome = nome;
+    }
+}
+
+
+public string 
