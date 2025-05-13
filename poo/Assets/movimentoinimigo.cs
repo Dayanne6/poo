19dadfaeeb9ffbeabeaa65a7c3a3567ebfbe6653
Dayanne6 <d.karoline@escolar.ifrn.edu.br>
2025-05-13 11:31:13 -0300

    13/05/2025

---
+++ poo/Assets/movimentoinimigo.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class movimentoinimigo : MonoBehaviour
+{
+    private GameObject _player;
+    
+    private Rigidbody _rigidBody;
+    private float velocidade;
+
+    public float realDivisao = 3;
+    void Start()
+    {
+        velocidade = gameObject.GetComponent<inimigodoper>().Velocidade();
+        _rigidBody = gameObject.GetComponent<Rigidbody>();
+        
+        
+        _player = GameObject.FindWithTag("Player");
+    }
+
+
+
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, _player.transform.position) < realDivisao)
+
+        {
+            transform.LookAt(_player.transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, velocidade * Time.deltaTime);
+        }
+    }
+}
---
+++ poo/Assets/player.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody _rigdibody;
+
+    private float velocidade = 10;
+    
+
+    void Start()
+    {
+        velocidade = gameObject.GetComponent<Personagem>().Velocidade();
+        _rigdibody = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        Vector3 posicao = transform.position;
+
+
+
+        // esquerda
+        if (Input.GetKey(KeyCode.D))
+        {
+            posicao.x = posicao.x + velocidade * Time.deltaTime;
+        }
+
+
+        //direita
+
+        if (Input.GetKey(KeyCode.A))
+            
+            
+            
+        {
+            posicao.x = transform.position.x - velocidade * Time.deltaTime;
+        }
+
+
+        // cima Z+
+        if (Input.GetKey(KeyCode.W))
+        {
+            posicao.z = transform.position.z + velocidade * Time.deltaTime;
+        }
+
+        //baixo Z-
+        if (Input.GetKey(KeyCode.S))
+        {
+            posicao.z = transform.position.z - velocidade * Time.deltaTime;
+
+        }
+        
+        transform.position = posicao;
+    }
+}
